8f12bc0c4a2b49eec00546f5836be4b08ba352f6
momo10101 <mohammad.jowkar@nem.software>
2021-08-24 10:35:01 +0200

    Feat: Added io, ionet and thread unit tests. Removed RemoteDiagnosticApi and its dependecies

--- tests/shared/core/HashTestUtils.h
+++ tests/shared/core/HashTestUtils.h
@@ -20,13 +20,23 @@
 **/
 
 #pragma once
-#include "symbol/exceptions.h"
+#include "symbol/core/model/RangeTypes.h"
+#include <vector>
 
-namespace catapult { namespace api {
+namespace catapult { namespace test {
 
-	/// Exception class that is thrown when a catapult api operation produces an error.
-	class catapult_api_error : public catapult_runtime_error {
-	public:
-		using catapult_runtime_error::catapult_runtime_error;
-	};
+	/// Generates a vector of offsets for a given nunber of hashes (\a numHashes).
+	std::vector<size_t> GenerateOffsets(size_t numHashes);
+
+	/// Generates \a numHashes random hashes.
+	model::HashRange GenerateRandomHashes(size_t numHashes);
+
+	/// Generates a subset of hashes consisting of the first \a numHashes hashes in \a source.
+	model::HashRange GenerateRandomHashesSubset(const model::HashRange& source, size_t numHashes);
+
+	/// Inserts all hashes from \a source into the destination vector (\a dest).
+	void InsertAll(std::vector<Hash256>& dest, const model::HashRange& source);
+
+	/// Concatenates two entity ranges specified by \a lhs and \a rhs.
+	model::HashRange ConcatHashes(const model::HashRange& lhs, const model::HashRange& rhs);
 }}
